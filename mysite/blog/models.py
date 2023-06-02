40aaf33c1fd443b1ce32976eb0c9413dc13f9a9f
RewoLF3020 <zolotovvladislav305@gmail.com>
2023-06-02 17:25:20 +0300

    canonical urls added

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 
@@ -18,7 +19,8 @@ class Post(models.Model):
         
     
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
     author = models.ForeignKey(User,
                               on_delete=models.CASCADE,
                               related_name='blog_posts')
@@ -29,6 +31,7 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+    
     objects = models.Manager()
     published = PublishedManager()
     
@@ -42,4 +45,12 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args=[self.publish.year,
+                              self.publish.month,
+                              self.publish.day,
+                              self.slug])
     
